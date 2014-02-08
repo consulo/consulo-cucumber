70e1b214b02717b2babc63fe3986aae0b3b5b8b5
VISTALL <vistall.valeriy@gmail.com>
2014-02-09 01:21:27 +0200

    compilation fixed

--- src/org/jetbrains/plugins/cucumber/psi/structure/GherkinStructureViewFactory.java
+++ src/org/jetbrains/plugins/cucumber/psi/structure/GherkinStructureViewFactory.java
@@ -1,36 +1,41 @@
 package org.jetbrains.plugins.cucumber.psi.structure;
 
-import com.intellij.ide.structureView.StructureViewBuilder;
-import com.intellij.ide.structureView.StructureViewModel;
-import com.intellij.ide.structureView.StructureViewModelBase;
-import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
-import com.intellij.lang.PsiStructureViewFactory;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.plugins.cucumber.psi.GherkinFeature;
 import org.jetbrains.plugins.cucumber.psi.GherkinFile;
 import org.jetbrains.plugins.cucumber.psi.GherkinStep;
 import org.jetbrains.plugins.cucumber.psi.GherkinStepsHolder;
+import com.intellij.ide.structureView.StructureViewBuilder;
+import com.intellij.ide.structureView.StructureViewModel;
+import com.intellij.ide.structureView.StructureViewModelBase;
+import com.intellij.ide.structureView.TreeBasedStructureViewBuilder;
+import com.intellij.lang.PsiStructureViewFactory;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
 
 /**
  * @author yole
  */
-public class GherkinStructureViewFactory implements PsiStructureViewFactory {
+public class GherkinStructureViewFactory implements PsiStructureViewFactory
+{
 	@Override
-	public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile) {
-		return new TreeBasedStructureViewBuilder() {
+	public StructureViewBuilder getStructureViewBuilder(final PsiFile psiFile)
+	{
+		return new TreeBasedStructureViewBuilder()
+		{
 			@NotNull
 			@Override
-			public StructureViewModel createStructureViewModel() {
+			public StructureViewModel createStructureViewModel(Editor editor)
+			{
 				PsiElement root = PsiTreeUtil.getChildOfType(psiFile, GherkinFeature.class);
-				if (root == null) {
+				if(root == null)
+				{
 					root = psiFile;
 				}
-				return
-						new StructureViewModelBase(psiFile, new GherkinStructureViewElement(root))
-								.withSuitableClasses(GherkinFile.class, GherkinFeature.class, GherkinStepsHolder.class, GherkinStep.class);
+				return new StructureViewModelBase(psiFile, new GherkinStructureViewElement(root)).withSuitableClasses(GherkinFile.class,
+						GherkinFeature.class, GherkinStepsHolder.class, GherkinStep.class);
 			}
 		};
 	}
