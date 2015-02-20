9bc08e09c780f57dc9946ca1f2780f55532822af
VISTALL <vistall.valeriy@gmail.com>
2015-02-21 01:55:32 +0200

    compilation

--- src/org/jetbrains/plugins/cucumber/psi/GherkinPsiManager.java
+++ src/org/jetbrains/plugins/cucumber/psi/GherkinPsiManager.java
@@ -1,80 +1,59 @@
 package org.jetbrains.plugins.cucumber.psi;
 
-import com.intellij.openapi.components.ProjectComponent;
+import org.jetbrains.annotations.NotNull;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiFileSystemItem;
-import com.intellij.psi.impl.AbstractModificationTracker;
-import com.intellij.psi.impl.PsiManagerImpl;
-import com.intellij.psi.impl.PsiModificationTrackerImpl;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.psi.impl.PsiTreeChangePreprocessorBase;
 
 /**
  * @author Roman.Chernyatchik
  */
-public class GherkinPsiManager extends AbstractModificationTracker implements ProjectComponent {
-  private PsiModificationTrackerImpl myModificationTracker;
-  protected Project myProject;
+public class GherkinPsiManager extends PsiTreeChangePreprocessorBase
+{
+	public GherkinPsiManager(@NotNull Project project)
+	{
+		super(project);
+	}
 
-  public GherkinPsiManager(@NotNull final Project project, @NotNull final PsiManagerImpl psiManager) {
-    super(psiManager);
-    myProject = project;
-  }
+	@Override
+	protected boolean isMyFile(@NotNull PsiFile file)
+	{
+		return file instanceof GherkinFile;
+	}
 
-  public static GherkinPsiManager getInstance(@NotNull final Project project) {
-    return project.getComponent(GherkinPsiManager.class);
-  }
+	@Override
+	protected boolean isInsideCodeBlock(PsiElement element)
+	{
+		if(element instanceof PsiFileSystemItem)
+		{
+			return false;
+		}
 
-  public void projectOpened() {
-    // Do nothing
-  }
+		if(element == null || element.getParent() == null)
+		{
+			return true;
+		}
 
-  public void projectClosed() {
-    // Do nothing
-  }
-
-  @NotNull
-  public String getComponentName() {
-    return "GherkinPsiManager";
-  }
-
-  public void initComponent() {
-    initTracker();
-  }
-
-  public void disposeComponent() {
-    // Do nothing
-  }
-
-
-  @Override
-  protected boolean isInsideCodeBlock(PsiElement element) {
-    if (element instanceof PsiFileSystemItem) {
-      return false;
-    }
-
-    if (element == null || element.getParent() == null) return true;
-
-    while(true) {
-      if (element instanceof GherkinFile
-          || element instanceof GherkinStep
-          || element instanceof GherkinFeature
-          || element instanceof GherkinStepsHolder) {
-        return false;
-      }
-      if (element instanceof PsiFile || element instanceof PsiDirectory || element == null) {
-        return true;
-      }
-      PsiElement parent = element.getParent();
-      if (parent instanceof GherkinTable
-          || parent instanceof GherkinExamplesBlock
-          || parent instanceof GherkinTableRow) {
-        return true;
-      }
-      element = parent;
-    }
-  }
-
+		while(true)
+		{
+			if(element instanceof GherkinFile || element instanceof GherkinStep || element instanceof GherkinFeature || element instanceof
+					GherkinStepsHolder)
+			{
+				return false;
+			}
+			if(element instanceof PsiFile || element instanceof PsiDirectory || element == null)
+			{
+				return true;
+			}
+			PsiElement parent = element.getParent();
+			if(parent instanceof GherkinTable || parent instanceof GherkinExamplesBlock || parent instanceof GherkinTableRow)
+			{
+				return true;
+			}
+			element = parent;
+		}
+	}
 }
